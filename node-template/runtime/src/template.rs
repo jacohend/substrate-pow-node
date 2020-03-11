752acda80a344f0c89e8d8089e6380c0403077c5
Jacob Henderson <jacobhenderson@Jacobs-MBP.attlocal.net>
2020-03-10 22:53:47 -0400

    post system

--- node-template/runtime/src/template.rs
+++ node-template/runtime/src/template.rs
@@ -7,14 +7,14 @@
 
 /// For more guidance on Substrate modules, see the example module
 /// https://github.com/paritytech/substrate/blob/master/srml/example/src/lib.rs
-
-use support::{decl_module, decl_storage, decl_event, dispatch::Result};
+use support::{decl_module, decl_storage, decl_event, ensure, dispatch::Result};
+use sr_primitives::traits::{Hash};
 use system::ensure_signed;
+use rstd::prelude::Vec;
 
 /// The module's configuration trait.
 pub trait Trait: system::Trait {
 	// TODO: Add other types and constants required configure this module.
-
 	/// The overarching event type.
 	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 }
@@ -22,34 +22,31 @@ pub trait Trait: system::Trait {
 // This module's storage items.
 decl_storage! {
 	trait Store for Module<T: Trait> as TemplateModule {
-		// Just a dummy storage item.
-		// Here we are declaring a StorageValue, `Something` as a Option<u32>
-		// `get(fn something)` is the default getter which returns either the stored `u32` or `None` if nothing stored
-		Something get(fn something): Option<u32>;
+		pub Posts get(post): map T::Hash => (T::AccountId, T::BlockNumber, Vec<u8>);
 	}
 }
 
 // The module's dispatchable functions.
 decl_module! {
+
 	/// The module declaration.
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
 		// Initializing events
 		// this is needed only if you are using events in your module
 		fn deposit_event() = default;
 
-		// Just a dummy entry point.
-		// function that can be called by the external world as an extrinsics call
-		// takes a parameter of the type `AccountId`, stores it and emits an event
-		pub fn do_something(origin, something: u32) -> Result {
+		pub fn create_post(origin, post: Vec<u8>) -> Result {
 			// TODO: You only need this if you want to check it was signed.
 			let who = ensure_signed(origin)?;
+            let current_block = <system::Module<T>>::block_number();
+            let mut postid = T::Hashing::hash_of(&post);
 
-			// TODO: Code to execute when something calls this.
+            ensure!(!Posts::<T>::exists(&postid), "This post has already been made.");
 			// For example: the following line stores the passed in u32 in the storage
-			Something::put(something);
+            Posts::<T>::insert(postid, (who.clone(), current_block, post));
 
 			// here we are raising the Something event
-			Self::deposit_event(RawEvent::SomethingStored(something, who));
+			Self::deposit_event(RawEvent::PostCreated(who.clone(), postid.as_mut().to_vec()));
 			Ok(())
 		}
 	}
@@ -60,7 +57,7 @@ decl_event!(
 		// Just a dummy event.
 		// Event `Something` is declared with a parameter of the type `u32` and `AccountId`
 		// To emit this event, we call the deposit funtion, from our runtime funtions
-		SomethingStored(u32, AccountId),
+		PostCreated(AccountId, Vec<u8>),
 	}
 );
 
